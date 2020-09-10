1d3763a11d691583751f62cac9e6f432b2e23512
MaxNguyen5040 <MaxNguyen5040>
2020-09-10 14:04:28 -0500

    Made an SRC file.

--- src/TurtleDemo.java
+++ src/TurtleDemo.java
@@ -1,15 +1,14 @@
 import java.awt.Color;
 
-public class TurtleDemo
-{
-    public static void main(String[] args)
-    {
-        //hi
-        //helllo
+public class TurtleDemo{
+
+    public static void main(String[] args) {
+
         World ocean = new World();
         Turtle crush = new Turtle(ocean);
         crush.setPenColor(Color.ORANGE);
         crush.penDown();
         crush.forward(50);
+
     }
 }
---
+++ src/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args){
+        int y = 0;
+        System.out.println(y);
+    }
+}
