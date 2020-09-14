6dc2e9b5dd51b14d52d7c3e2fd24919db30169ae
MaxNguyen5040 <MaxNguyen5040>
2020-09-14 14:27:06 -0500

    Put in Hello 1+2 files

---
+++ src/Hello1.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class Hello1
+{
+    public static void main(String[] args)
+    {
+        String name = JOptionPane.showInputDialog("What is your name?");
+        System.out.println(name);
+    }
+}
