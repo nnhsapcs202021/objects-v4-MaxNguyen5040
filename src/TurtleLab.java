bb55ed1043175b228dcfd0aca5a9b94ff6baba55
MaxNguyen5040 <MaxNguyen5040>
2020-09-29 14:06:51 -0500

    Added a recursive method for TurtleLab

--- src/TurtleLab.java
+++ src/TurtleLab.java
@@ -2,13 +2,31 @@ import java.awt.Color;
 
 public class TurtleLab
 {
+
     public static void main(String[] args)
     {
         World Earth = new World();
         Turtle bob = new Turtle(Earth);
-        Turtle rob = new Turtle(Earth);
-        bob.forward(20);
-        rob.backward(20);
+        //Turtle rob = new Turtle(Earth);
+        triangleMaker(1, bob,100);
 
     }
+
+    public static int triangleMaker(int side_length, Turtle turtle_name, int end_side_length){
+        if(side_length == end_side_length) {
+            return 1;
+        }
+
+        else {
+            for (int i = 0; i < 3; i++) {
+                turtle_name.forward(side_length);
+                turtle_name.turn(120); }
+            turtle_name.turn(1);
+            return triangleMaker(side_length + 1,turtle_name,end_side_length);
+
+        }
+
+    }
+
+
 }
